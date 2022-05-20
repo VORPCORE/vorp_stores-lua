84940627434d1b755128d91d36af7b5ca9f7a58f
Outsider <87246847+outsider31000@users.noreply.github.com>
2022-05-20 23:56:33 +0100

    version check

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -21,8 +21,9 @@ dependencies {
     'vorp_inputs' -- download from the vorp github
 }
 
---dont touch
---version '1.0'
---vorp_checker 'yes'
----vorp_name '^4Resource version Check^3'
---vorp_github 'https://github.com/VORPCORE/vorp_stores-lua'
+dont
+touch
+version '1.0'
+vorp_checker 'yes'
+vorp_name '^4Resource version Check^3'
+vorp_github 'https://github.com/VORPCORE/vorp_stores-lua'
