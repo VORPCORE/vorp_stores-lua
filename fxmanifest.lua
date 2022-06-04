039fe97c5dbdec353ef30ae5f283685d501424a4
Outsider <87246847+outsider31000@users.noreply.github.com>
2022-06-04 01:16:07 +0100

    update version

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -26,7 +26,7 @@ dependencies {
 --dont
 --touch
 
-version '1.5'
+version '1.6'
 vorp_checker 'yes'
 vorp_name '^4Resource version Check^3'
 vorp_github 'https://github.com/VORPCORE/vorp_stores-lua'
