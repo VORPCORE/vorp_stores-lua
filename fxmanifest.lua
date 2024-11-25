f05ad54ba9613bb560e84ce7781f2315cb0277c2
Outsider <87246847+outsider31000@users.noreply.github.com>
2024-11-25 17:32:38 +0000

    version 2.2

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,10 +1,9 @@
 fx_version 'cerulean'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
-
 game 'rdr3'
+name 'vorp stores'
 author 'VORP @outsider31000'
 description 'A store script for vorp core framework'
-repository 'https://github.com/VORPCORE/vorp_stores-lua'
 lua54 'yes'
 
 
@@ -20,7 +19,7 @@ client_script 'client/client.lua'
 server_script 'server/server.lua'
 
 
-version '2.1'
+version '2.2'
 vorp_checker 'yes'
 vorp_name '^4Resource version Check^3'
 vorp_github 'https://github.com/VORPCORE/vorp_stores-lua'
