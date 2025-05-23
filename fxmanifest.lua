5a945c1484080ce3f017044b0ea6691f2c3fb37b
Outsider <87246847+outsider31000@users.noreply.github.com>
2025-05-23 17:20:42 +0100

    version 2.4

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -19,7 +19,7 @@ client_script 'client/client.lua'
 server_script 'server/server.lua'
 
 
-version '2.3'
+version '2.4'
 vorp_checker 'yes'
 vorp_name '^4Resource version Check^3'
 vorp_github 'https://github.com/VORPCORE/vorp_stores-lua'
